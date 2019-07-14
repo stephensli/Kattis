3b0e2fd9068192d5fa7e27f01f4585e4c552af06
Stephen Lineker-Miller <slinekermiller@gmail.com>
2019-07-14 21:58:22 +0100

    completed challenges

--- batterup.cpp
+++ batterup.cpp
@@ -8,22 +8,22 @@ int main(int argc, char* argv[])
 {
 	double number_bats;
 	double output_value = 0;
-
+    
 	cin >> number_bats;
-
+    
 	double divider = number_bats;
-
+    
 	for (int i = 0; i < number_bats; ++i)
 	{
 		double input_value;
 		cin >> input_value;
-
+        
 		if (input_value == -1) divider -= 1;
 		else output_value += input_value;
 	}
-
+    
 	double divider_value = output_value / divider;
-
+    
 	std::cout << setprecision(16);
 	std::cout << divider_value << endl;
 }
